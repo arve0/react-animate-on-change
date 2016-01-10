5f1b0f77821ba6d970c3c03f09b270844b3c4635
arve0 <arve.seljebu@gmail.com>
2016-01-10 10:23:42 +0100

    remove event listener on unmount

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-
 /**
  * # AnimateOnChange component.
  * Adds `animationClassName` when `animate` is true, then removes
@@ -12,42 +11,61 @@ import React, { Component } from 'react'
  * @prop {bool} animate - Wheter to animate component.
  */
 class AnimateOnChange extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = { animating: false, clearAnimationClass: false }
+    this.animationStart = this.animationStart.bind(this)
+    this.animationEnd = this.animationEnd.bind(this)
   }
 
-  componentDidMount() {
-    this.refs.root.addEventListener('animationstart', () => {
-      this.setState({ animating: true, clearAnimationClass: false })
-    })
-    this.refs.root.addEventListener('animationend', () => {
-      // send separate, animation state change will not render
-      this.setState({ clearAnimationClass: true })  // renders
-      this.setState({ animating: false, clearAnimationClass: false })
-    })
+  componentDidMount () {
+    const elm = this.refs.root
+    elm.addEventListener('animationstart', this.animationStart)
+    elm.addEventListener('animationend', this.animationEnd)
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.animating != nextState.animating){
+  componentWillUnmount () {
+    const elm = this.refs.root
+    elm.removeEventListener('animationstart', this.animationStart)
+    elm.removeEventListener('animationend', this.animationEnd)
+  }
+
+  animationStart () {
+    this.setState({ animating: true, clearAnimationClass: false })
+  }
+
+  animationEnd () {
+    // send separate, animation state change will not render
+    this.setState({ clearAnimationClass: true })  // renders
+    this.setState({ animating: false, clearAnimationClass: false })
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (this.state.animating !== nextState.animating) {
       // do not render on animation change
       return false
     }
     return true
   }
 
-  render() {
+  render () {
     let className = this.props.baseClassName
 
     if (this.props.animate && !this.state.clearAnimationClass) {
       className += ` ${this.props.animationClassName}`
     }
 
-    return <span ref="root" className={className}>
+    return <span ref='root' className={className}>
       {this.props.children}
     </span>
   }
 }
 
+AnimateOnChange.propTypes = {
+  children: React.PropTypes.element.isRequired,
+  animate: React.PropTypes.bool.isRequired,
+  baseClassName: React.PropTypes.string.isRequired,
+  animationClassName: React.PropTypes.string.isRequired
+}
 
 export default AnimateOnChange
