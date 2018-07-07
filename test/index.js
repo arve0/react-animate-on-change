4e836f28c5ba5e585e6cb1b85f59dfd7aee63e2f
Arve Seljebu <arve.seljebu@gmail.com>
2018-07-07 10:21:16 +0200

    transpile to es5

--- test/index.js
+++ test/index.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -7,8 +17,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importStar(require("react"));
-const events = {
+var react_1 = __importStar(require("react"));
+var events = {
     start: ['animationstart', 'webkitAnimationStart', 'mozAnimationStart', 'oanimationstart', 'MSAnimationStart'],
     end: ['animationend', 'webkitAnimationEnd', 'mozAnimationEnd', 'oanimationend', 'MSAnimationEnd'],
     startRemoved: [],
@@ -24,52 +34,54 @@ const events = {
  * @prop {string} animationClassName - Class added when `animate == true`.
  * @prop {bool} animate - Wheter to animate component.
  */
-class AnimateOnChange extends react_1.Component {
-    constructor(props) {
-        super(props);
-        this.state = { animating: false, clearAnimationClass: false };
-        this.animationStart = this.animationStart.bind(this);
-        this.animationEnd = this.animationEnd.bind(this);
-        this.setElementRef = (ref) => {
-            this.elm = ref;
+var AnimateOnChange = /** @class */ (function (_super) {
+    __extends(AnimateOnChange, _super);
+    function AnimateOnChange(props) {
+        var _this = _super.call(this, props) || this;
+        _this.state = { animating: false, clearAnimationClass: false };
+        _this.animationStart = _this.animationStart.bind(_this);
+        _this.animationEnd = _this.animationEnd.bind(_this);
+        _this.setElementRef = function (ref) {
+            _this.elm = ref;
         };
+        return _this;
     }
-    componentDidMount() {
+    AnimateOnChange.prototype.componentDidMount = function () {
         this.addEventListener('start', this.elm, this.animationStart);
         this.addEventListener('end', this.elm, this.animationEnd);
-    }
-    componentWillUnmount() {
+    };
+    AnimateOnChange.prototype.componentWillUnmount = function () {
         this.removeEventListeners('start', this.elm, this.animationStart);
         this.removeEventListeners('end', this.elm, this.animationEnd);
-    }
-    addEventListener(type, elm, eventHandler) {
+    };
+    AnimateOnChange.prototype.addEventListener = function (type, elm, eventHandler) {
         // until an event has been triggered bind them all
-        events[type].map(event => {
+        events[type].map(function (event) {
             // console.log(`adding ${event}`)
             // @ts-ignore
             elm.addEventListener(event, eventHandler);
         });
-    }
-    removeEventListeners(type, elm, eventHandler) {
-        events[type].map(event => {
+    };
+    AnimateOnChange.prototype.removeEventListeners = function (type, elm, eventHandler) {
+        events[type].map(function (event) {
             // console.log(`removing ${event}`)
             // @ts-ignore
             elm.removeEventListener(event, eventHandler);
         });
-    }
-    updateEvents(type, newEvent) {
+    };
+    AnimateOnChange.prototype.updateEvents = function (type, newEvent) {
         // console.log(`updating ${type} event to ${newEvent}`)
-        events[type + 'Removed'] = events[type].filter(e => e !== newEvent);
+        events[type + 'Removed'] = events[type].filter(function (e) { return e !== newEvent; });
         events[type] = [newEvent];
-    }
-    animationStart(e) {
+    };
+    AnimateOnChange.prototype.animationStart = function (e) {
         if (events['start'].length > 1) {
             this.updateEvents('start', e.type);
             this.removeEventListeners('startRemoved', this.elm, this.animationStart);
         }
         this.setState({ animating: true, clearAnimationClass: false });
-    }
-    animationEnd(e) {
+    };
+    AnimateOnChange.prototype.animationEnd = function (e) {
         if (events['end'].length > 1) {
             this.updateEvents('end', e.type);
             this.removeEventListeners('endRemoved', this.elm, this.animationStart);
@@ -80,21 +92,22 @@ class AnimateOnChange extends react_1.Component {
         if (typeof this.props.onAnimationEnd === 'function') {
             this.props.onAnimationEnd();
         }
-    }
-    shouldComponentUpdate(nextProps, nextState) {
+    };
+    AnimateOnChange.prototype.shouldComponentUpdate = function (nextProps, nextState) {
         if (this.state.animating !== nextState.animating) {
             // do not render on animation change
             return false;
         }
         return true;
-    }
-    render() {
-        let className = this.props.baseClassName;
+    };
+    AnimateOnChange.prototype.render = function () {
+        var className = this.props.baseClassName;
         if (this.props.animate && !this.state.clearAnimationClass) {
-            className += ` ${this.props.animationClassName}`;
+            className += " " + this.props.animationClassName;
         }
-        let Tag = this.props.customTag || 'span';
+        var Tag = this.props.customTag || 'span';
         return react_1.default.createElement(Tag, { ref: this.setElementRef, className: className }, this.props.children);
-    }
-}
+    };
+    return AnimateOnChange;
+}(react_1.Component));
 exports.default = AnimateOnChange;
